4e63014dedd72dcc9e58b0270e847f6222cf62b0
Kim Ji Yun <62929727+yoondotcom@users.noreply.github.com>
2020-06-22 21:49:26 +0900

    2020.06.22 실습예제

---
+++ edu/edu/src/com/edu/test/SecondServlet.java
@@ -0,0 +1,23 @@
+package com.edu.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class SecondServlet extends HttpServlet{
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter out = resp.getWriter();
+		out.print("<h1>SecondServlet service()!!");
+		out.print("코로나 제발 가라!!");
+		out.close();
+	}
+}
